f8858cdd99e4c851984e6ea3feb1c5d65b0e6199
salmannasir119 <123494200+salmannasir119@users.noreply.github.com>
2023-01-24 22:54:39 +0500

    Add files via upload

---
+++ HTML CSS Training/script.js
@@ -0,0 +1,21 @@
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function CheckPassword(inputtxt) 
+{ 
+var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+if(inputtxt.value.match(paswd)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}  
